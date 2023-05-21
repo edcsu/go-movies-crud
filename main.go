40b3161cca5a7cc95c74747c218d987158dfca88
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-05-21 12:53:30 +0300

    feat: add delete movie handler

--- main.go
+++ main.go
@@ -27,6 +27,17 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -34,6 +45,7 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Isbn: "12909", Title: "Dynasty In Namibia", Director: &Director{firstname: "Jane", lastname: "Doe"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
